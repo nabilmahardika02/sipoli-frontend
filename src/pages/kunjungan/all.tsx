f47a0d8dcbd156661defa47a737c77449ec7c8ff
mazaya.nur11 <mazaya.nur11@ui.ac.id>
2024-10-05 17:36:40 +0700

    fix: filter daftar kunjungan

--- src/pages/kunjungan/all.tsx
+++ src/pages/kunjungan/all.tsx
@@ -12,7 +12,7 @@ import { checkRole } from "@/lib/checkRole";
 import Forbidden from "@/components/fragments/Forbidden";
 import Input from "@/components/elements/forms/Input";
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
-import { SandboxForm } from "@/types/forms/SandboxForm";
+import { FilterKunjunganForm } from "@/types/forms/filterKunjunganForm";
 import axios from "axios";
 import Typography from "@/components/elements/Typography";
 
@@ -22,7 +22,7 @@ const KunjunganAllPage = () => {
   const [startDate, setStartDate] = useState<Date>();
   const [endDate, setEndDate] = useState<Date>();
 
-  const methods = useForm<SandboxForm>({
+  const methods = useForm<FilterKunjunganForm>({
     mode: "onTouched",
   });
   
@@ -53,43 +53,39 @@ const KunjunganAllPage = () => {
     fetchKunjungans();
   }, []);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault(); // Mencegah reload halaman
-    await onSubmit();
-  };
+  const { handleSubmit } = methods;
 
-  const onSubmit = async () => {
-    try {
-      const response = await axios.get(`/kunjungan/all`, {
-        params: {
-          startDate: startDate,
-          endDate: endDate,
-        },
-      });
-      setKunjungans(response.data.result); // Mengupdate state dengan data yang diterima
-    } catch (error) {
-      console.error("Error fetching kunjungan:", error);
-    }
-  };
+  const onSubmit: SubmitHandler<FilterKunjunganForm> = (data) => {
+    const postData = async () => {
+      const [responseData, message, isSuccess] = await sendRequest(
+        "get",
+        "kunjungan/all?startDate="+data.startDate+"&endDate="+data.endDate
+      );
+
+      if (isSuccess) {
+        setKunjungans(responseData as Kunjungan[]);
+      }
+    };
+
+    postData();
+    };
 
   return (
     <main>
       <section className="mt-5">
         <FormProvider {...methods}>
-          <form onSubmit={handleSubmit} className="my-5">
+          <form onSubmit={handleSubmit(onSubmit)} className="my-5">
           <div className="flex justify-center items-center gap-2">
             <Input
               id="startDate"
               label="Tanggal Awal"
               type="date"
-              onChange={(e) => setStartDate(e.target.value)}
             />
             <Typography variant="p4">sampai</Typography>
             <Input
               id="endDate"
               label="Tanggal Akhir"
               type="date"
-              onChange={(e) => setEndDate(e.target.value)}
             />
             <Button type="submit" className="h-full py-[calc(0.375rem+1px)] px-4">Cari</Button>
             </div>
